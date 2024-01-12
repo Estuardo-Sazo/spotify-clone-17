87d66092288827347da4f273f44b37528244e9d6
Estuardo-Sazo <estuardosazo1206@gmail.com>
2024-01-12 01:05:42 -0600

    Modulos con paginas generados

---
+++ src/app/modules/favorites/pages/favorite-page/favorite-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-favorite-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './favorite-page.component.html',
+  styleUrl: './favorite-page.component.scss'
+})
+export class FavoritePageComponent {
+
+}
