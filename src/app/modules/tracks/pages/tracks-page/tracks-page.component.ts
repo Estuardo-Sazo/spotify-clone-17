29d1614c410eaefcb9f52e7cf508dd7571aee69a
Estuardo-Sazo <estuardosazo1206@gmail.com>
2024-01-12 22:57:42 -0600

    Crear componetnes Card y seccion generica

--- src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
+++ src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
-import { TracksModule } from '@modules/tracks/tracks.module';
+import { SectionGenericComponent } from '@shared/components/section-generic/section-generic.component';
 
 @Component({
   selector: 'app-tracks-page',
   standalone: true,
-  imports: [TracksModule],
+  imports: [SectionGenericComponent],
   templateUrl: './tracks-page.component.html',
   styleUrl: './tracks-page.component.scss'
 })
 export class TracksPageComponent {
+ mockTracksList = [
+  {
+    name: 'Canción 1',
+    album: 'Album 1',
+    cover: 'https://picsum.photos/200/300',
+    url: 'https://picsum.photos/200/300',
+    _id:1,
+  },
+  {
+    name: 'Canción 2',
+    album: 'Album 2',
+    cover: 'https://picsum.photos/200/300',
+    url: 'https://picsum.photos/200/300',
+    _id:2,
+  },
+  {
+    name: 'Canción 3',
+    album: 'Album 3',
+    cover: 'https://picsum.photos/200/300',
+    url: 'https://picsum.photos/200/300',
+    _id:3,
+  }
+ ];
 
 }
--- src/app/shared/components/section-generic/section-generic.component.ts
+++ src/app/shared/components/section-generic/section-generic.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { TrackModel } from '@core/models/tracks.model';
+import { CardPlayerComponent } from '../card-player/card-player.component';
 
 @Component({
   selector: 'app-section-generic',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, CardPlayerComponent],
   templateUrl: './section-generic.component.html',
   styleUrl: './section-generic.component.scss'
 })
 export class SectionGenericComponent {
-
+  @Input() title: string='';
+  @Input() mode: 'small' | 'big' = 'big'
+  @Input() dataTracks: Array<TrackModel> = []
 }
