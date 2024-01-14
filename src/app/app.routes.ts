0593e498bdf6e514132fbe9876c56a6d91bf19d3
Estuardo-Sazo <estuardosazo1206@gmail.com>
2024-01-13 22:52:54 -0600

    Vista principal de tracks

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,26 +1,44 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {
-        path: 'auth',
-        loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./modules/auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('@modules/home/pages/home-page/home-page.component').then(
+        (m) => m.HomePageComponent
+      ),
+    children: [
+      {
+        path: 'tracks',
+        loadComponent: () =>
+          import(
+            '@modules/tracks/pages/tracks-page/tracks-page.component'
+          ).then((m) => m.TracksPageComponent),
       },
       {
-        path: 'home',
-        loadComponent: () => import('@modules/home/pages/home-page/home-page.component').then(m => m.HomePageComponent),
-        children: [
-          {
-            path: 'tracks',
-            loadComponent:() => import('@modules/tracks/pages/tracks-page/tracks-page.component').then(m => m.TracksPageComponent)
-          },
-          {
-            path: 'favorites',
-            loadComponent:() => import('@modules/favorites/pages/favorite-page/favorite-page.component').then(m => m.FavoritePageComponent)
-          },
-          {
-            path: 'history',
-            loadComponent:() => import('@modules/history/pages/history-page/history-page.component').then(m => m.HistoryPageComponent)
-          },
-        ]
+        path: 'favorites',
+        loadComponent: () =>
+          import(
+            '@modules/favorites/pages/favorite-page/favorite-page.component'
+          ).then((m) => m.FavoritePageComponent),
       },
+      {
+        path: 'history',
+        loadComponent: () =>
+          import(
+            '@modules/history/pages/history-page/history-page.component'
+          ).then((m) => m.HistoryPageComponent),
+      },
+    ],
+  },
 ];
--- src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
+++ src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
@@ -1,36 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SectionGenericComponent } from '@shared/components/section-generic/section-generic.component';
+import * as dataRaw from '../../../../data/tracks.json';
+import { TrackModel } from '@core/models/tracks.model';
 
 @Component({
   selector: 'app-tracks-page',
   standalone: true,
   imports: [SectionGenericComponent],
   templateUrl: './tracks-page.component.html',
-  styleUrl: './tracks-page.component.scss'
+  styleUrl: './tracks-page.component.scss',
 })
-export class TracksPageComponent {
- mockTracksList = [
-  {
-    name: 'Canción 1',
-    album: 'Album 1',
-    cover: 'https://picsum.photos/200/300',
-    url: 'https://picsum.photos/200/300',
-    _id:1,
-  },
-  {
-    name: 'Canción 2',
-    album: 'Album 2',
-    cover: 'https://picsum.photos/200/300',
-    url: 'https://picsum.photos/200/300',
-    _id:2,
-  },
-  {
-    name: 'Canción 3',
-    album: 'Album 3',
-    cover: 'https://picsum.photos/200/300',
-    url: 'https://picsum.photos/200/300',
-    _id:3,
+export class TracksPageComponent implements OnInit {
+  mockTracksList: Array<TrackModel> = [];
+  ngOnInit(): void {
+    const {data}: any = (dataRaw as any).default;
+    this.mockTracksList = data;
   }
- ];
-
 }
